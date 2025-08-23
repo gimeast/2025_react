729ab4de264efa36aa4234787aa04c4b82d4c10c
gimeast <slasnrndu@gmail.com>
2025-08-23 23:44:50 +0900

    코딩애플 리액트 학습

--- shop/src/pages/Detail.jsx
+++ shop/src/pages/Detail.jsx
@@ -1,15 +1,47 @@
 import { shoesData } from "../data.js";
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Detail() {
+    const [isVisible, setIsVisible] = useState(true);
+    const [number, setNumber] = useState(null);
+
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const id = params.get("id");
     const { title, content, price } = shoesData[id];
+
+    useEffect(() => {
+        if (number !== null && Number.isNaN(number)) {
+            alert("그러지마세요");
+            setNumber("");
+        }
+    }, [number]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div>
+            {isVisible && (
+                <div className="text-red-300 font-bold text-2xl">
+                    2초이내 구매시 할인
+                </div>
+            )}
+
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" />
             <div className="col-md-6">
+                <input
+                    className="border"
+                    type="text"
+                    value={number}
+                    onChange={(e) => setNumber(Number(e.target.value))}
+                />
                 <h4 className="pt-5">{title}</h4>
                 <p>{content}</p>
                 <p>{price.toLocaleString()}원</p>
--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -1,34 +1,61 @@
 import { useState } from "react";
 import { shoesData } from "../data.js";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
     const [shoes, setShoes] = useState(shoesData);
+    const [isPending, setIsPending] = useState(false);
+    const [count, setCount] = useState(2);
+
+    function onClick() {
+        setIsPending(true);
+        axios
+            .get(`https://codingapple1.github.io/shop/data${count}.json`)
+            .then((result) => {
+                setShoes((prevState) => [...prevState, ...result.data]);
+                setCount((prevState) => prevState + 1);
+            })
+            .catch(() => {
+                console.log("네트워크 통신 실패");
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    console.log("loading");
+                    setIsPending(false);
+                }, 1000);
+            });
+    }
 
     return (
         <>
             <div className="h-[300px] w-dvw bg-[gray] bg-center bg-cover bg-[url(https://codingapple1.github.io/shop/shoes1.jpg)]"></div>
             <div className="flex flex-wrap gap-10 w-full justify-center text-center mt-30">
-                {shoes.map((data) => (
-                    <Link
-                        key={data.id}
-                        className="bg-[lightgreen] w-1/3 min-w-[300px] max-w-[300px] h-[300px] flex flex-col items-center rounded-xl"
-                        to={`/detail?id=${data.id}`}
-                    >
-                        <div className="bg-[gray] w-[250px] h-[140px] mt-4">
-                            <img
-                                src="https://codingapple1.github.io/shop/shoes1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <h2 className="text-2xl font-[500] mt-10">
-                            {data.title}
-                        </h2>
-                        <p>{data.content}</p>
-                        <span>{data.price.toLocaleString()}</span>
-                    </Link>
-                ))}
+                {isPending && <span>로딩중입니다.</span>}
+                {!isPending &&
+                    shoes.map((data) => (
+                        <Link
+                            key={data.id}
+                            className="bg-[lightgreen] w-1/3 min-w-[300px] max-w-[300px] h-[300px] flex flex-col items-center rounded-xl"
+                            to={`/detail?id=${data.id}`}
+                        >
+                            <div className="bg-[gray] w-[250px] h-[140px] mt-4">
+                                <img
+                                    src="https://codingapple1.github.io/shop/shoes1.jpg"
+                                    alt=""
+                                />
+                            </div>
+                            <h2 className="text-2xl font-[500] mt-10">
+                                {data.title}
+                            </h2>
+                            <p>{data.content}</p>
+                            <span>{data.price.toLocaleString()}</span>
+                        </Link>
+                    ))}
             </div>
+            <button className="border cursor-pointer" onClick={onClick}>
+                더보기 버튼
+            </button>
         </>
     );
 }
